5dc0fe396fdacf4145794c31e80763faa8b9d061
nada-hassan <60989613+nada-hassan@users.noreply.github.com>
2021-09-13 20:46:51 +0200

    Add files via upload

--- src/components/Modalbox.js
+++ src/components/Modalbox.js
@@ -1,10 +1,14 @@
 import { Modal, Button } from "react-bootstrap";
 import { useState } from 'react'
 
-const Modalbox =() => {
+const Modalbox =(props) => {
   const [isOpen, setState] = useState(false)
   const openModal = () => { setState(true) };
   const closeModal = () => { setState(false) };
+  const completeCheckout = () => {
+    closeModal();
+   alert('Your purchase is completed. A confirmation e-mail with all your purchase informaion has been sent to you to track your delivery');
+  }
     return (
       <>
           <Button variant="primary" onClick={openModal}>Checkout
@@ -13,21 +17,22 @@ const Modalbox =() => {
           <Modal.Header closeButton>
             <Modal.Title>Checkout</Modal.Title>
           </Modal.Header>
-
+          <form>
           <Modal.Body>            
-            <input type="text" placeholder="Your Full name"/>
-            <input type="text" placeholder="Your E-mail" />
-            <input type="text" placeholder="Your phone number" />
-            <input type="text" placeholder="Your Country" />
-            <input type="text" placeholder="Your Address" />
+            <input className="info" type="text" placeholder="Your Full name" required/>
+            <input className="info" type="text" placeholder="Your E-mail" required/>
+            <input className="info" type="text" placeholder="Your phone number" required/>
+            <input className="info" type="text" placeholder="Your Country" required/>
+            <input className="info" type="text" placeholder="Your Address" required/>
             </Modal.Body>
 
           <Modal.Footer>
-            <Button variant="secondary" onClick={closeModal}>
+              <Button style={{ backgroundColor: "darkblue" }} className="btn" variant="secondary" onClick={completeCheckout}>
               Complete Checkout
             </Button>
+           
           </Modal.Footer>
-          
+          </form>
         </Modal>
       </>
 
